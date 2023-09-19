0a5692b9074e846cd722c13f111223f8fa128f98
Isaac Song <isaacsongs10@gmail.com>
2023-09-18 23:30:46 -0700

    Add form submit function

--- src/index.js
+++ src/index.js
@@ -20,21 +20,21 @@ document.addEventListener("DOMContentLoaded", () => {
 //! Fetch data and render to page
 fetch("http://localhost:3000/toys")
   .then(resp => resp.json())
-  .then(movieObj =>  movieObj.forEach(movie => renderToy(movie)))
+  .then(toyObj =>  toyObj.forEach(toy => renderToy(toy)))
   .catch(error => alert(`You ran into this error: ${error.textContent}`))
 
 //! Helper Functions
-function renderToy(movie) {
+function renderToy(toy) {
   const div = document.createElement("div");
   div.className = "card";
   const h2 = document.createElement("h2");
-  h2.textContent = movie.name;
+  h2.textContent = toy.name;
   const image = document.createElement("img");
   image.className = "toy-avatar";
-  image.src = movie.image;
-  image.alt = movie.name;
+  image.src = toy.image;
+  image.alt = toy.name;
   const likes = document.createElement("p");
-  likes.textContent = `${movie.likes} Likes`;
+  likes.textContent = `${toy.likes} Likes`;
   const btn = document.createElement("button");
   btn.className = "like-btn";
   btn.textContent = "Like ❤️";
@@ -42,3 +42,36 @@ function renderToy(movie) {
   document.querySelector("#toy-collection").append(div);
 }
 
+function validateToyData(valuesArr) {
+  return valuesArr.every(el => el.trim() !== "");
+}
+
+// Might want to add if Toy already exists, then do not create toy
+function handleSubmit(event) {
+  event.preventDefault();
+  if (validateToyData([event.target.name.value, event.target.image.value])) {
+    const newToy = {
+      name: event.target.name.value,
+      image: event.target.image.value,
+      likes: '0 Likes',
+    }
+
+    renderToy(newToy);
+
+    fetch("http://localhost:3000/toys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newToy)
+    })
+    .then(resp => {
+      if (!resp.ok) {
+        alert("There was an error in the response");
+      }
+    })
+    .catch(error => alert(error))
+    } else {
+      alert("You must fill out the boxes!");
+    }
+}
